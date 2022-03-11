5b4f2b9981b15532a2a4fbc152a2b4bf11404d5d
Alejandro Rivas <alexrivas502@gmail.com>
2022-03-11 00:57:40 -0600

    Update hello.py
    Updated to the second version

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ DEFAULT_WORLD = os.environ.get(
 
 def hello(world: Optional[str] = None):
     world = world or DEFAULT_WORLD
-    return f"Hello, {world}"
+    return f"Hello, {world}, this is the second version of the script!"
 
 
 if __name__ == "__main__":
