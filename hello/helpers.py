5d9489ef2a003ebcc46be8727057e68de12d5364
Alejandro Rivas <alexrivas502@gmail.com>
2022-03-11 01:14:35 -0600

    Created requirements and helpers

---
+++ hello/helpers.py
@@ -0,0 +1,16 @@
+from typing import Optional, Dict
+
+
+class HelperFunctions:
+
+    def concat_strings(str1: str, str2: str) -> str:
+        """This functions concatenates strings
+
+        Args:
+            str1 (str): First string to concatenate
+            str2 (str): Second string to concatenate
+
+        Returns:
+            str: Concatenated string
+        """
+        return f"{str1}{str2}"
